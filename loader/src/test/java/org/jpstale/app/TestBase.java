8a6e06e21b9e246c4e92e2a0de5b6f73629f4850
闫茂源 <yanmaoyuan@baijiayun.com>
2023-08-30 09:18:02 +0800

    [M]修复高版本jme3的测试环境。

--- loader/src/test/java/org/jpstale/app/TestBase.java
+++ loader/src/test/java/org/jpstale/app/TestBase.java
@@ -3,6 +3,7 @@ package org.jpstale.app;
 
 import com.jme3.app.SimpleApplication;
 
+import com.jme3.system.AppSettings;
 import org.jpstale.assets.AssetFactory;
 
 /**
@@ -12,6 +13,12 @@ import org.jpstale.assets.AssetFactory;
  *
  */
 public abstract class TestBase extends SimpleApplication {
+
+    TestBase() {
+        super();
+        AppSettings settings = new AppSettings(true);
+        super.setSettings(settings);
+    }
 
     @Override
     public void simpleInitApp() {
