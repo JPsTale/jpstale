7d30b86d3def6b741cdfb5e7689fd46673bda32f
闫茂源 <115050813@qq.com>
2026-03-14 12:25:11 +0800

    feat: 修改ItemDB和UserDB的字段定义。

--- server/postgres/ItemDB_create.sql
+++ server/postgres/ItemDB_create.sql
@@ -1,90 +1,101 @@
-CREATE TABLE Item(
-	ID integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
-	CharcterID integer NOT NULL,
-	Location integer NOT NULL,
-	Data bytea NOT NULL,
-	Header integer NOT NULL,
-	Timestamp integer NOT NULL,
-	Checksum integer NOT NULL,
-	BackupHeader integer NOT NULL,
-	BackupChecksum integer NOT NULL,
-	Flags integer NOT NULL,
-	CreateTime timestamp without time zone NOT NULL,
-	DeleteTime timestamp without time zone NOT NULL
+-- 创建 schema
+CREATE SCHEMA IF NOT EXISTS itemdb;
+
+-- 设置搜索路径（可选）
+SET search_path TO itemdb, public;
+
+-- 物品表
+CREATE TABLE itemdb.item (
+    id integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    character_id integer NOT NULL,
+    location integer NOT NULL,
+    data bytea NOT NULL,
+    header integer NOT NULL,
+    timestamp integer NOT NULL,
+    checksum integer NOT NULL,
+    backup_header integer NOT NULL,
+    backup_checksum integer NOT NULL,
+    flags integer NOT NULL,
+    create_time timestamp without time zone NOT NULL,
+    delete_time timestamp without time zone NOT NULL
 );
 
-CREATE TABLE ItemBase(
-	ItemBaseId integer NOT NULL PRIMARY KEY,
-	ItemBaseName varchar(20) NOT NULL,
-	ItemBaseHex bytea NOT NULL,
-	ItemBaseAbbrv varchar(3) NULL
+-- 物品基础定义
+CREATE TABLE itemdb.item_base (
+    item_base_id integer NOT NULL PRIMARY KEY,
+    item_base_name varchar(20) NOT NULL,
+    item_base_hex bytea NOT NULL,
+    item_base_abbrv varchar(3) NULL
 );
 
-CREATE TABLE ItemCraftType(
-	ItemCraftTypeId smallint NOT NULL PRIMARY KEY,
-	ItemCraftTypeName varchar(15) NOT NULL
+-- 物品制作类型
+CREATE TABLE itemdb.item_craft_type (
+    item_craft_type_id smallint NOT NULL PRIMARY KEY,
+    item_craft_type_name varchar(15) NOT NULL
 );
 
-CREATE TABLE ItemDump(
-	Id integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
-	AccountName varchar(32) NULL,
-	CharName varchar(32) NULL,
-	ItemName varchar(50) NOT NULL,
-	ItemLevel integer NOT NULL,
-	ItemId integer NOT NULL,
-	ItemTypeId integer NOT NULL,
-	ItemBaseId integer NOT NULL,
-	ItemHeader integer NOT NULL,
-	ItemChecksum integer NOT NULL,
-	ItemBackupHeader integer NOT NULL,
-	ItemBackupChecksum integer NOT NULL,
-	MixEffect integer NOT NULL,
-	MixID integer NOT NULL,
-	AgeLevel integer NOT NULL,
-	AttackRange integer NOT NULL,
-	AttackSpeed integer NOT NULL,
-	AttackRating integer NOT NULL,
-	ATKPowMin smallint NOT NULL,
-	ATKPowMax smallint NOT NULL,
-	Critical integer NULL,
-	Absorb double precision NOT NULL,
-	Defense integer NOT NULL,
-	Block double precision NOT NULL,
-	Organic smallint NOT NULL,
-	Fire smallint NOT NULL,
-	Frost smallint NOT NULL,
-	Lighting smallint NOT NULL,
-	Poison smallint NOT NULL,
-	StrengthReq integer NOT NULL,
-	SpiritReq integer NOT NULL,
-	TalentReq integer NOT NULL,
-	AgilityReq integer NOT NULL,
-	HealthReq integer NOT NULL,
-	MPRegen double precision NOT NULL,
-	HPRegen double precision NOT NULL,
-	SPRegen double precision NOT NULL,
-	AddHP double precision NOT NULL,
-	AddMP double precision NOT NULL,
-	AddSP double precision NOT NULL,
-	ItemSpec integer NOT NULL,
-	SpecAbsorb double precision NOT NULL,
-	SpecDef integer NOT NULL,
-	SpecBlockRating double precision NOT NULL,
-	SpecAttackSpeed integer NOT NULL,
-	SpecCritical integer NOT NULL,
-	SpecAttackRatingDiv smallint NOT NULL,
-	SpecAttackPowerDiv smallint NOT NULL,
-	SpecMPRegen double precision NOT NULL,
-	SpecHPRegen double precision NOT NULL,
-	SpecSPRegen double precision NOT NULL,
-	ItemUniqueID integer NOT NULL,
-	SalePrice integer NOT NULL,
-	CreatedDate integer NOT NULL
+-- 物品转储日志
+CREATE TABLE itemdb.item_dump (
+    id integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    account_name varchar(32) NULL,
+    char_name varchar(32) NULL,
+    item_name varchar(50) NOT NULL,
+    item_level integer NOT NULL,
+    item_id integer NOT NULL,
+    item_type_id integer NOT NULL,
+    item_base_id integer NOT NULL,
+    item_header integer NOT NULL,
+    item_checksum integer NOT NULL,
+    item_backup_header integer NOT NULL,
+    item_backup_checksum integer NOT NULL,
+    mix_effect integer NOT NULL,
+    mix_id integer NOT NULL,
+    age_level integer NOT NULL,
+    attack_range integer NOT NULL,
+    attack_speed integer NOT NULL,
+    attack_rating integer NOT NULL,
+    atk_pow_min smallint NOT NULL,
+    atk_pow_max smallint NOT NULL,
+    critical integer NULL,
+    absorb double precision NOT NULL,
+    defense integer NOT NULL,
+    block double precision NOT NULL,
+    organic smallint NOT NULL,
+    fire smallint NOT NULL,
+    frost smallint NOT NULL,
+    lighting smallint NOT NULL,
+    poison smallint NOT NULL,
+    strength_req integer NOT NULL,
+    spirit_req integer NOT NULL,
+    talent_req integer NOT NULL,
+    agility_req integer NOT NULL,
+    health_req integer NOT NULL,
+    mp_regen double precision NOT NULL,
+    hp_regen double precision NOT NULL,
+    sp_regen double precision NOT NULL,
+    add_hp double precision NOT NULL,
+    add_mp double precision NOT NULL,
+    add_sp double precision NOT NULL,
+    item_spec integer NOT NULL,
+    spec_absorb double precision NOT NULL,
+    spec_def integer NOT NULL,
+    spec_block_rating double precision NOT NULL,
+    spec_attack_speed integer NOT NULL,
+    spec_critical integer NOT NULL,
+    spec_attack_rating_div smallint NOT NULL,
+    spec_attack_power_div smallint NOT NULL,
+    spec_mp_regen double precision NOT NULL,
+    spec_hp_regen double precision NOT NULL,
+    spec_sp_regen double precision NOT NULL,
+    item_unique_id integer NOT NULL,
+    sale_price integer NOT NULL,
+    created_date integer NOT NULL
 );
 
-CREATE TABLE ItemType(
-	ItemTypeId integer NOT NULL PRIMARY KEY,
-	ItemTypeName varchar(20) NOT NULL,
-	ItemTypeHex bytea NOT NULL,
-	ItemTypeAbbrv varchar(3) NULL
+-- 物品类型
+CREATE TABLE itemdb.item_type (
+    item_type_id integer NOT NULL PRIMARY KEY,
+    item_type_name varchar(20) NOT NULL,
+    item_type_hex bytea NOT NULL,
+    item_type_abbrv varchar(3) NULL
 );
--- server/postgres/UserDB_create.sql
+++ server/postgres/UserDB_create.sql
@@ -1,226 +1,238 @@
-DROP TABLE CharacterExpDef;
-CREATE TABLE CharacterExpDef(
-	Level integer NOT NULL,
-	ExpRequired bigint NOT NULL
+-- 创建 schema
+CREATE SCHEMA IF NOT EXISTS userdb;
+
+SET search_path TO userdb, public;
+
+-- 角色经验定义
+CREATE TABLE userdb.character_exp_def (
+    level integer NOT NULL,
+    exp_required bigint NOT NULL
 );
 
-CREATE TABLE CharacterInfo(
-	ID integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
-	AccountName varchar(32) NOT NULL,
-	Name varchar(32) NOT NULL,
-	OldHead varchar(64) NOT NULL,
-	Level integer NOT NULL,
-	Experience bigint NOT NULL,
-	Gold integer NOT NULL,
-	JobCode integer NOT NULL,
-	ClanID integer NOT NULL,
-	ClanPermission integer NOT NULL,
-	ClanLeaveDate integer NOT NULL,
-	LastSeenDate timestamp without time zone NOT NULL,
-	BlessCastleScore integer NOT NULL,
-	FSP integer NOT NULL,
-	LastStage integer NOT NULL,
-	IsOnline integer NOT NULL,
-	Seasonal integer NOT NULL,
-	GMLevel integer NOT NULL,
-	Banned integer NOT NULL,
-	Title integer NULL,
-	DialogSkin integer NULL,
-	LevelUpDate timestamp without time zone NULL
+-- 角色信息
+CREATE TABLE userdb.character_info (
+    id integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    account_name varchar(32) NOT NULL,
+    name varchar(32) NOT NULL,
+    old_head varchar(64) NOT NULL,
+    level integer NOT NULL,
+    experience bigint NOT NULL,
+    gold integer NOT NULL,
+    job_code integer NOT NULL,
+    clan_id integer NOT NULL,
+    clan_permission integer NOT NULL,
+    clan_leave_date integer NOT NULL,
+    last_seen_date timestamp without time zone NOT NULL,
+    bless_castle_score integer NOT NULL,
+    fsp integer NOT NULL,
+    last_stage integer NOT NULL,
+    is_online integer NOT NULL,
+    seasonal integer NOT NULL,
+    gm_level integer NOT NULL,
+    banned integer NOT NULL,
+    title integer NULL,
+    dialog_skin integer NULL,
+    level_up_date timestamp without time zone NULL
 );
 
-CREATE TABLE CharacterInfoDelete(
-	ID integer NOT NULL PRIMARY KEY,
-	AccountName varchar(32) NOT NULL,
-	Name varchar(32) NOT NULL,
-	OldHead varchar(64) NOT NULL,
-	Level integer NOT NULL,
-	Experience bigint NOT NULL,
-	Gold integer NOT NULL,
-	JobCode integer NOT NULL,
-	ClanID integer NOT NULL,
-	ClanPermission integer NOT NULL,
-	ClanLeaveDate integer NOT NULL,
-	LastSeenDate varchar(50) NOT NULL,
-	BlessCastleScore integer NOT NULL,
-	FSP integer NOT NULL,
-	LastStage integer NOT NULL,
-	IsOnline integer NOT NULL,
-	Seasonal integer NOT NULL,
-	GMLevel integer NOT NULL,
-	Banned integer NOT NULL,
-	Title integer NULL,
-	DialogSkin integer NULL
+-- 已删除角色信息
+CREATE TABLE userdb.character_info_delete (
+    id integer NOT NULL PRIMARY KEY,
+    account_name varchar(32) NOT NULL,
+    name varchar(32) NOT NULL,
+    old_head varchar(64) NOT NULL,
+    level integer NOT NULL,
+    experience bigint NOT NULL,
+    gold integer NOT NULL,
+    job_code integer NOT NULL,
+    clan_id integer NOT NULL,
+    clan_permission integer NOT NULL,
+    clan_leave_date integer NOT NULL,
+    last_seen_date varchar(50) NOT NULL,
+    bless_castle_score integer NOT NULL,
+    fsp integer NOT NULL,
+    last_stage integer NOT NULL,
+    is_online integer NOT NULL,
+    seasonal integer NOT NULL,
+    gm_level integer NOT NULL,
+    banned integer NOT NULL,
+    title integer NULL,
+    dialog_skin integer NULL
 );
 
-CREATE TABLE CharacterItemTimer(
-    ID integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
-	CharacterID integer NOT NULL,
-	CharacterName varchar(32) NOT NULL,
-	ItemTimerType integer NOT NULL,
-	ItemID integer NOT NULL,
-	TimeLeft integer NOT NULL,
-	TimeTotal integer NOT NULL
+-- 角色物品计时器
+CREATE TABLE userdb.character_item_timer (
+    id integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    character_id integer NOT NULL,
+    character_name varchar(32) NOT NULL,
+    item_timer_type integer NOT NULL,
+    item_id integer NOT NULL,
+    time_left integer NOT NULL,
+    time_total integer NOT NULL
 );
 
-CREATE TABLE CharacterMonsterKillTracker(
-	ID integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
-	CharacterID integer NOT NULL,
-	MonsterID integer NOT NULL,
-	MonsterName varchar(32) NOT NULL,
-	KillCount integer NOT NULL
+-- 角色击杀统计
+CREATE TABLE userdb.character_monster_kill_tracker (
+    id integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    character_id integer NOT NULL,
+    monster_id integer NOT NULL,
+    monster_name varchar(32) NOT NULL,
+    kill_count integer NOT NULL
 );
 
-CREATE TABLE CharacterPvP(
-	ID integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
-	CharacterName varchar(32) NOT NULL,
-	Kills integer NOT NULL,
-	Deaths integer NOT NULL,
-	KillStreak integer NOT NULL,
-	TopPvP integer NOT NULL
+-- 角色PVP统计
+CREATE TABLE userdb.character_pvp (
+    id integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    character_name varchar(32) NOT NULL,
+    kills integer NOT NULL,
+    deaths integer NOT NULL,
+    kill_streak integer NOT NULL,
+    top_pvp integer NOT NULL
 );
 
-CREATE TABLE CharacterQuest(
-	ID integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
-	QuestID integer NOT NULL,
-	QuestRewardID integer NOT NULL,
-	CharacterID integer NOT NULL,
-	AccountName varchar(32) NOT NULL,
-	CharacterName varchar(32) NOT NULL,
-	StartDate timestamp without time zone NOT NULL,
-	EndDate timestamp without time zone NULL,
-	MonsterCount varchar(64) NULL,
-	ItemCount varchar(64) NULL,
-	TimeLeft integer NOT NULL,
-	Counter integer NULL
+-- 角色任务
+CREATE TABLE userdb.character_quest (
+    id integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    quest_id integer NOT NULL,
+    quest_reward_id integer NOT NULL,
+    character_id integer NOT NULL,
+    account_name varchar(32) NOT NULL,
+    character_name varchar(32) NOT NULL,
+    start_date timestamp without time zone NOT NULL,
+    end_date timestamp without time zone NULL,
+    monster_count varchar(64) NULL,
+    item_count varchar(64) NULL,
+    time_left integer NOT NULL,
+    counter integer NULL
 );
 
-CREATE TABLE CharacterTitleList(
-	ID integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
-	CharacterID integer NOT NULL,
-	TitleID integer NOT NULL,
-	DateAcquired timestamp without time zone NOT NULL
+-- 角色称号列表
+CREATE TABLE userdb.character_title_list (
+    id integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    character_id integer NOT NULL,
+    title_id integer NOT NULL,
+    date_acquired timestamp without time zone NOT NULL
 );
 
-CREATE TABLE ClassDef(
-	ClassID integer NOT NULL PRIMARY KEY,
-	ClassName varchar(20) NOT NULL
+-- 职业定义
+CREATE TABLE userdb.class_def (
+    class_id integer NOT NULL PRIMARY KEY,
+    class_name varchar(20) NOT NULL
 );
 
-CREATE TABLE ItemBox(
-	ID integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
-	AccountName varchar(32) NOT NULL,
-	ItemCode varchar(32) NOT NULL,
-	Item varchar(32) NULL,
-	ItemSpec integer NOT NULL,
-	CoinShop integer NOT NULL,
-	HasItem integer NULL,
-	DateReceived timestamp without time zone NULL
+-- 物品箱
+CREATE TABLE userdb.item_box (
+    id integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    account_name varchar(32) NOT NULL,
+    item_code varchar(32) NOT NULL,
+    item varchar(32) NULL,
+    item_spec integer NOT NULL,
+    coin_shop integer NOT NULL,
+    has_item integer NULL,
+    date_received timestamp without time zone NULL
 );
 
-CREATE TABLE NotificationData(
-	ID integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
-	AccountName varchar(32) NOT NULL,
-	Message varchar(100) NOT NULL,
-	Checked integer NOT NULL
+-- 通知数据
+CREATE TABLE userdb.notification_data (
+    id integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    account_name varchar(32) NOT NULL,
+    message varchar(100) NOT NULL,
+    checked integer NOT NULL
 );
 
-CREATE TABLE TitleDef(
-	TitleID integer NOT NULL PRIMARY KEY,
-	TitleCategory varchar(16) NOT NULL,
-	SubCategory varchar(16) NULL,
-	Title varchar(21) NOT NULL,
-	TitleRarity smallint NOT NULL
-);
-CREATE TABLE UserInfo(
-	ID integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
-	AccountName varchar(32) NOT NULL,
-	Password varchar(64) NOT NULL,
-	RegisDay varchar(50) NOT NULL,
-	Flag integer NOT NULL,
-	Active integer NOT NULL,
-	ActiveCode varchar(50) NOT NULL,
-	Coins integer NOT NULL,
-	Email varchar(150) NOT NULL,
-	GameMasterType integer NOT NULL,
-	GameMasterLevel integer NOT NULL,
-	GameMasterMacAddress varchar(20) NOT NULL,
-	CoinsTraded integer NOT NULL,
-	BanStatus integer NOT NULL,
-	UnbanDate timestamp without time zone NULL,
-	IsMuted integer NOT NULL,
-	MuteCount integer NOT NULL,
-	UnmuteDate timestamp without time zone NULL
+-- 称号定义
+CREATE TABLE userdb.title_def (
+    title_id integer NOT NULL PRIMARY KEY,
+    title_category varchar(16) NOT NULL,
+    sub_category varchar(16) NULL,
+    title varchar(21) NOT NULL,
+    title_rarity smallint NOT NULL
 );
 
--- 存储过程：根据莫里普（MonsterID=1258）击杀数授予称号（50~54）
-CREATE OR REPLACE PROCEDURE public.GiveWantedMoriphTitlesBasedOnKills()
+-- 用户信息
+CREATE TABLE userdb.user_info (
+    id integer GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    account_name varchar(32) NOT NULL,
+    password varchar(64) NOT NULL,
+    regis_day varchar(50) NOT NULL,
+    flag integer NOT NULL,
+    active integer NOT NULL,
+    active_code varchar(50) NOT NULL,
+    coins integer NOT NULL,
+    email varchar(150) NOT NULL,
+    game_master_type integer NOT NULL,
+    game_master_level integer NOT NULL,
+    game_master_mac_address varchar(20) NOT NULL,
+    coins_traded integer NOT NULL,
+    ban_status integer NOT NULL,
+    unban_date timestamp without time zone NULL,
+    is_muted integer NOT NULL,
+    mute_count integer NOT NULL,
+    unmute_date timestamp without time zone NULL
+);
+
+CREATE OR REPLACE PROCEDURE userdb.give_wanted_moriph_titles_based_on_kills()
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    -- 清除旧的称号记录
-    DELETE FROM public.CharacterTitleList WHERE titleid IN (50, 51, 52, 53, 54);
+    DELETE FROM userdb.character_title_list WHERE title_id IN (50, 51, 52, 53, 54);
 
-    -- 插入各档位称号
-    INSERT INTO public.CharacterTitleList (characterid, titleid, dateacquired)
-    SELECT characterid, 50, CURRENT_TIMESTAMP
-    FROM public.CharacterMonsterKillTracker
-    WHERE monsterid = 1258 AND killcount > 50;
+    INSERT INTO userdb.character_title_list (character_id, title_id, date_acquired)
+    SELECT character_id, 50, CURRENT_TIMESTAMP
+    FROM userdb.character_monster_kill_tracker
+    WHERE monster_id = 1258 AND kill_count > 50;
 
-    INSERT INTO public.CharacterTitleList (characterid, titleid, dateacquired)
-    SELECT characterid, 51, CURRENT_TIMESTAMP
-    FROM public.CharacterMonsterKillTracker
-    WHERE monsterid = 1258 AND killcount > 250;
+    INSERT INTO userdb.character_title_list (character_id, title_id, date_acquired)
+    SELECT character_id, 51, CURRENT_TIMESTAMP
+    FROM userdb.character_monster_kill_tracker
+    WHERE monster_id = 1258 AND kill_count > 250;
 
-    INSERT INTO public.CharacterTitleList (characterid, titleid, dateacquired)
-    SELECT characterid, 52, CURRENT_TIMESTAMP
-    FROM public.CharacterMonsterKillTracker
-    WHERE monsterid = 1258 AND killcount > 500;
+    INSERT INTO userdb.character_title_list (character_id, title_id, date_acquired)
+    SELECT character_id, 52, CURRENT_TIMESTAMP
+    FROM userdb.character_monster_kill_tracker
+    WHERE monster_id = 1258 AND kill_count > 500;
 
-    INSERT INTO public.CharacterTitleList (characterid, titleid, dateacquired)
-    SELECT characterid, 53, CURRENT_TIMESTAMP
-    FROM public.CharacterMonsterKillTracker
-    WHERE monsterid = 1258 AND killcount > 1250;
+    INSERT INTO userdb.character_title_list (character_id, title_id, date_acquired)
+    SELECT character_id, 53, CURRENT_TIMESTAMP
+    FROM userdb.character_monster_kill_tracker
+    WHERE monster_id = 1258 AND kill_count > 1250;
 
-    INSERT INTO public.CharacterTitleList (characterid, titleid, dateacquired)
-    SELECT characterid, 54, CURRENT_TIMESTAMP
-    FROM public.CharacterMonsterKillTracker
-    WHERE monsterid = 1258 AND killcount > 2500;
+    INSERT INTO userdb.character_title_list (character_id, title_id, date_acquired)
+    SELECT character_id, 54, CURRENT_TIMESTAMP
+    FROM userdb.character_monster_kill_tracker
+    WHERE monster_id = 1258 AND kill_count > 2500;
 END;
 $$;
 
--- 存储过程：根据狼（MonsterID=1253）击杀数授予称号（70~74）
-CREATE OR REPLACE PROCEDURE public.GiveWantedWolfTitlesBasedOnKills()
+CREATE OR REPLACE PROCEDURE userdb.give_wanted_wolf_titles_based_on_kills()
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    -- 清除旧的称号记录
-    DELETE FROM public.CharacterTitleList WHERE titleid IN (70, 71, 72, 73, 74);
+    DELETE FROM userdb.character_title_list WHERE title_id IN (70, 71, 72, 73, 74);
 
-    -- 插入各档位称号
-    INSERT INTO public.CharacterTitleList (characterid, titleid, dateacquired)
-    SELECT characterid, 70, CURRENT_TIMESTAMP
-    FROM public.CharacterMonsterKillTracker
-    WHERE monsterid = 1253 AND killcount > 50;
+    INSERT INTO userdb.character_title_list (character_id, title_id, date_acquired)
+    SELECT character_id, 70, CURRENT_TIMESTAMP
+    FROM userdb.character_monster_kill_tracker
+    WHERE monster_id = 1253 AND kill_count > 50;
 
-    INSERT INTO public.CharacterTitleList (characterid, titleid, dateacquired)
-    SELECT characterid, 71, CURRENT_TIMESTAMP
-    FROM public.CharacterMonsterKillTracker
-    WHERE monsterid = 1253 AND killcount > 250;
+    INSERT INTO userdb.character_title_list (character_id, title_id, date_acquired)
+    SELECT character_id, 71, CURRENT_TIMESTAMP
+    FROM userdb.character_monster_kill_tracker
+    WHERE monster_id = 1253 AND kill_count > 250;
 
-    INSERT INTO public.CharacterTitleList (characterid, titleid, dateacquired)
-    SELECT characterid, 72, CURRENT_TIMESTAMP
-    FROM public.CharacterMonsterKillTracker
-    WHERE monsterid = 1253 AND killcount > 500;
+    INSERT INTO userdb.character_title_list (character_id, title_id, date_acquired)
+    SELECT character_id, 72, CURRENT_TIMESTAMP
+    FROM userdb.character_monster_kill_tracker
+    WHERE monster_id = 1253 AND kill_count > 500;
 
-    INSERT INTO public.CharacterTitleList (characterid, titleid, dateacquired)
-    SELECT characterid, 73, CURRENT_TIMESTAMP
-    FROM public.CharacterMonsterKillTracker
-    WHERE monsterid = 1253 AND killcount > 1250;
+    INSERT INTO userdb.character_title_list (character_id, title_id, date_acquired)
+    SELECT character_id, 73, CURRENT_TIMESTAMP
+    FROM userdb.character_monster_kill_tracker
+    WHERE monster_id = 1253 AND kill_count > 1250;
 
-    INSERT INTO public.CharacterTitleList (characterid, titleid, dateacquired)
-    SELECT characterid, 74, CURRENT_TIMESTAMP
-    FROM public.CharacterMonsterKillTracker
-    WHERE monsterid = 1253 AND killcount > 2500;
+    INSERT INTO userdb.character_title_list (character_id, title_id, date_acquired)
+    SELECT character_id, 74, CURRENT_TIMESTAMP
+    FROM userdb.character_monster_kill_tracker
+    WHERE monster_id = 1253 AND kill_count > 2500;
 END;
 $$;
